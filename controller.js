da4e2c6fc669ae3ecfeebc5bee792275f019f32f
Jacob Tyler <jacobtyler22@icloud.com>
2015-01-21 11:55:09 -0700

    User Profiles 2 Project

--- controller.js
+++ controller.js
@@ -3,9 +3,10 @@ var app = angular.module('userProfiles');
 app.controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
+  	mainService.getUsers().then(function(res){
+  		$scope.users = res.data.data;
+  	});
+  };
 
   $scope.getUsers();
 
